ddee16862408e2184c7a161707eec968efe569d6
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2024-05-26 13:05:02 +0800

    Added SolidDemo solution

---
+++ src/MainApp/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿namespace MainApp;
+
+public class BankAccount
+{  
+    private decimal _balance;
+
+    public BankAccount(int accountId, string accountHolderName, decimal balance)
+    {
+        AccountId = accountId;
+        AccountHolderName = accountHolderName;
+        _balance = balance;
+    }
+
+    public int AccountId { get; private set; }
+    public string AccountHolderName { get; private set; }
+
+    public decimal GetBalance()
+    {
+        return _balance;
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        if (amount <= 0 || amount > _balance)
+        {
+            return false;
+        }
+
+        _balance -= amount;
+        LogTransaction(amount);
+        return true;
+    }
+
+    private void LogTransaction(decimal amount)
+    {
+        Console.WriteLine($"Withdrawal of {amount:C} from Account ID {AccountId}");
+    }
+}
+
---
+++ src/MainApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MainApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
